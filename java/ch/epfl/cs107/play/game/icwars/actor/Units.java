cdf30eb5bcc42285635f77d09d0eb3a7a055913e
brosio-lsn <ambroise.aigueperse@gmail.com>
2021-12-05 16:07:37 +0100

    range of the units created and is filled in the unit's constructor (cf 2.4)

--- java/ch/epfl/cs107/play/game/icwars/actor/Units.java
+++ java/ch/epfl/cs107/play/game/icwars/actor/Units.java
@@ -1,9 +1,14 @@
 package ch.epfl.cs107.play.game.icwars.actor;
 
 import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.Orientation;
+import ch.epfl.cs107.play.game.areagame.actor.Path;
 import ch.epfl.cs107.play.game.areagame.actor.Sprite;
+import ch.epfl.cs107.play.game.icwars.area.ICWarsRange;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.window.Canvas;
+
+import java.util.Queue;
 
 abstract public class Units extends ICWarsActor {
 
@@ -15,6 +20,8 @@ abstract public class Units extends ICWarsActor {
     // ui
     protected String name;
     protected Sprite sprite;
+    //path
+    ICWarsRange range;
 
     /**
      * @param area       the area in yhich the unit is
@@ -31,6 +38,7 @@ abstract public class Units extends ICWarsActor {
         this.radius = radius;
         this.setHp(current_HP);
         this.repair = repair;
+        completeUnitsRange();
     }
 
     protected abstract int getDammage();
@@ -65,7 +73,24 @@ abstract public class Units extends ICWarsActor {
         return this.name;
     }
 
-
+    /**
+     * fills the unit's range attribute with nodes that are both
+     * withtin a radius range of the unit's coordinatates and in the grid
+     */
+    private void completeUnitsRange(){
+        int fromX= this.getCurrentMainCellCoordinates().x;
+        int fromY=this.getCurrentMainCellCoordinates().y;
+        for(int x=-radius+1; x<radius;++x){
+            for(int y=-radius+1; y<radius;++y){
+                boolean hasLeftNeighbourg = x+fromX>0;
+                boolean hasRightNeighbourg=x+fromX<this.getOwnerArea().getWidth()-1;
+                boolean hasTopNeighbourg = y+fromY>0;
+                boolean hasUnderNeighbourg = y+fromY<this.getOwnerArea().getHeight()-1;
+                DiscreteCoordinates NodeCoordinates = new DiscreteCoordinates((int) this.getPosition().x, (int) this.getPosition().y);
+                range.addNode(NodeCoordinates, hasLeftNeighbourg,hasTopNeighbourg,hasRightNeighbourg,hasUnderNeighbourg);
+            }
+        }
+    }
     /**
      * a unit doesn't take spaceCellSpace
      */
@@ -88,5 +113,24 @@ abstract public class Units extends ICWarsActor {
     @Override
     public boolean isCellInteractable() {
         return true;
+    }
+
+    /**
+     * Draw the unit's range and a path from the unit position to
+     destination
+     * @param destination path destination
+     * @param canvas canvas
+     */
+    public void drawRangeAndPathTo(DiscreteCoordinates destination ,
+                                   Canvas canvas) {
+        range.draw(canvas);
+        Queue<Orientation> path =
+                range.shortestPath(getCurrentMainCellCoordinates(),
+                        destination);
+//Draw path only if it exists (destination inside the range)
+        if (path != null){
+            new Path(getCurrentMainCellCoordinates().toVector(),
+                    path).draw(canvas);
+        }
     }
 }
