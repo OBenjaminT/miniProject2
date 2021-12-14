f68fa6867e7fa620811e19f05fed3bd0ca95b42d
oliverT <oliver.turner@epfl.ch>
2021-12-14 18:27:48 +0100

    todo

--- java/ch/epfl/cs107/play/game/icwars/actor/RealPlayer.java
+++ java/ch/epfl/cs107/play/game/icwars/actor/RealPlayer.java
@@ -30,7 +30,12 @@ public class RealPlayer extends ICWarsPlayer {
      * TODO
      */
     private final ICWarsPlayerInteractionHandler handler = new ICWarsPlayerInteractionHandler(this);
+
+    /**
+     * TODO
+     */
     private Action ActionToExecute;
+
     /**
      * TODO
      *
